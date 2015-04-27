69e3c4193ebb8627d7484df6327c78cef2f53a38
Achilleas <achilleas.k@gmail.com>
2015-04-27 16:22:04 +0300

    Thesis figure changes.

--- plot_mu_fest_err.py
+++ plot_mu_fest_err.py
@@ -9,7 +9,7 @@ rc('text', usetex=True) # use latex
 '''
 Plot freq est err vs mu peak est err
 '''
-filename = 'lotsofdata.npz'
+filename = '/media/olddisks/simdata/ou_lif/lotsofdata.npz'
 archive = np.load(filename)
 mu_peaks_actual = archive['mu_peaks_actual']
 mu_peaks_est_bad = archive['mu_peaks_est_bad']
@@ -21,13 +21,14 @@ mu_peaks_est_err = array([mean(abs(mu_peaks_actual-mpeb)/mu_peaks_actual) for
                                 mpeb in mu_peaks_est_bad.transpose()])
 figure(num=1, figsize=(8, 6), dpi=100)
 clf()
-plot(fest_err, mu_peaks_est_err, 'k')
+plot(fest_err, mu_peaks_est_err)
 xlabel(r'$ \varepsilon_f $', size=15)
 ylabel(r'$ \varepsilon_{\mu_p} $', size=15)
-xticks(linspace(-0.1, 0.1, 11), size=15)
+xticks(linspace(-0.1, 0.1, 5), size=15)
 yticks(size=15)
-axis(xmin=-0.1, xmax=0.1)
-savefig('brain_research_figures/mu_err_vs_freq_err.png')
-savefig('brain_research_figures/mu_err_vs_freq_err.pdf')
+axis(xmin=-0.1, xmax=0.1, ymin=0)
+grid()
+savefig('figures/mu_err_vs_freq_err.png')
+savefig('figures/mu_err_vs_freq_err.pdf')
 print "Figure saved!"
 
